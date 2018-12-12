94f1aebbb6fb3e9bbd779ada4813a42102f0d496
yajerry19971997 <32739968+yajerry19971997@users.noreply.github.com>
2018-12-12 20:19:52 +0800

    Create s1.php

---
+++ s1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<div id="container" style="width:100%px">
+
+<div id="header" style="background-color:#FFFF00;">
+<h1 style="margin-bottom:0;">STU CSIE Try To Hacking</h1></div>
+
+<div id="menu" style="background-color:#c0c0c0;height:100%;width:15%;float:left;">
+<a href="http://192.168.0.114/s1.php" target="_blank" title="">第一關</a></br>
+<a href="https://www.facebook.com/" target="_blank" title="">第二關</a></br>
+<a href="https://www.facebook.com/" target="_blank" title="">第三關</a></br>
+<a href="https://www.facebook.com/" target="_blank" title="">第四關</a></br>
+<a href="https://www.facebook.com/" target="_blank" title="">第五關</a></br>
+<a href="https://www.facebook.com/" target="_blank" title="">第六關</a></br>
+<a href="https://www.facebook.com/" target="_blank" title="">第七關</a></br>
+<a href="https://www.facebook.com/" target="_blank" title="">第八關</a></br>
+<a href="https://www.facebook.com/" target="_blank" title="">第九關</a></br>
+<a href="https://www.facebook.com/" target="_blank" title="">第十關</a></br>
+<a href="http://192.168.0.114/index.php" target="_blank" title="">主頁</a></br>
+
+</div>
+
+<div id="content" style="background-color:#EEEEEE;height:100%;width:85%;float:left;">
+
+<iframe width="1280" height="720" src="https://www.youtube.com/embed/XA58vyTPrnM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+
+<div id="footer" style="background-color:#FFA500;clear:both;text-align:center;">
+版權所有 © Akira</div>
+
+</div>
+
+</body>
+</html>
